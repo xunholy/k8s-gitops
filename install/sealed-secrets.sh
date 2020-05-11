69db33e070811309aa35c2cffbbe7f1973a9af6d
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2020-05-11 20:20:54 +1000

    updating scripts
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- install/sealed-secrets.sh
+++ install/sealed-secrets.sh
@@ -2,8 +2,12 @@
 
 set -eou pipefail
 
-MASTER_KEY="../.k8s/sealedsecrets-key.yaml"
+MASTER_KEY="../.secrets/master-sealed-secret.yaml"
 
-kubectl delete secret -l sealedsecrets.bitnami.com/sealed-secrets-key=active -n sealed-secrets
-[[ -f ${MASTER_KEY} ]] && kubectl apply -f ${MASTER_KEY} || exit 1
-kubectl delete pod -l name=sealed-secrets-controller -n sealed-secrets
+kubectl delete secret -l sealedsecrets.bitnami.com/sealed-secrets-key=active -n kube-system
+if [[ -f ${MASTER_KEY} ]]; then
+  echo "Applying Sealed Secrets master key"
+  kubectl apply -f ${MASTER_KEY}
+fi
+
+kubectl delete pod -l name=sealed-secrets -n kube-system
