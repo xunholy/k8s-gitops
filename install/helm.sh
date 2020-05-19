5c3a99a27bf10311beeb58a3507bbc07683781aa
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2020-05-19 20:44:45 +1000

    install helm operator as part of the flux
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- install/helm.sh
+++ install/helm.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-
 set -eou pipefail
 
 # kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/v1.0.0/deploy/crds.yaml \
 #     --dry-run=none -o yaml > namespaces/flux/helm-operator.yaml
-curl -o namespaces/flux/helm-operator/helm-operator.yaml https://raw.githubusercontent.com/fluxcd/helm-operator/v1.0.0/deploy/crds.yaml
+curl -o namespaces/flux/helm-operator/helm-operator-crd.yaml https://raw.githubusercontent.com/fluxcd/helm-operator/v1.0.0/deploy/crds.yaml
+
+helm repo add fluxcd https://charts.fluxcd.io
 
 # Only Helm 3 support enabled using helm.versions
-helm upgrade -i helm-operator fluxcd/helm-operator \
+helm template fluxcd/helm-operator \
     --namespace flux \
-    --values=config/helm-operator/values.yaml
+    --values=config/helm-operator/values.yaml > namespaces/flux/helm-operator/helm-operator.yaml
