616e7571dca9ac8156bc6c498100e6c357d83b83
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2020-06-12 14:36:15 +1000

    updating helm-operator to manage its own crds and psp resources
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- install/helm-gen.sh
+++ install/helm-gen.sh
@@ -3,12 +3,11 @@ set -eou pipefail
 
 HELM_OPERATOR_VERSION=v1.1.0
 
-curl -sL "https://raw.githubusercontent.com/fluxcd/helm-operator/${HELM_OPERATOR_VERSION}/deploy/crds.yaml" -o namespaces/flux/helm-operator/000-crd.yaml
-
 mkdir -p output
 
 helm repo add fluxcd https://charts.fluxcd.io
 helm template fluxcd/helm-operator \
+    --version="${HELM_OPERATOR_VERSION}" \
     --name-template=default \
     --namespace flux \
     --values=config/helm-operator/values.yaml > output/helm-operator.yaml
