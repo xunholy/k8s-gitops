790d9882e94e4363d1f6211c27641e7f85d39756
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2020-08-12 18:43:42 +1000

    update flux to set specific version of chart and remove dependency installation (#83)
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- scripts/flux.sh
+++ scripts/flux.sh
@@ -5,23 +5,16 @@ set -eou pipefail
 CLEAN=${CLEAN:-true}
 
 if [[ ! $(fluxctl) ]]; then
-    LOCAL_OS=$(uname)
-    if [[ $LOCAL_OS == Darwin ]]; then
-        brew install fluxctl
-    elif [[ $LOCAL_OS == Linux ]]; then
-        sudo snap install fluxctl
-    else
-        echo "Fluxctl needs to be manually installed - https://docs.fluxcd.io/en/latest/references/fluxctl/"
-        exit 1
-    fi
+    echo "Fluxctl needs to be manually installed - https://docs.fluxcd.io/en/latest/references/fluxctl/"
+    exit 1
 fi
 
 # Untaint master nodes
 # TODO: Enable Ansible to allow configuring the taints to be added/removed.
-[[ ! $(kubectl taint nodes --all node-role.kubernetes.io/master-) ]] && echo "Masters untainted"
+[[ ! $(kubectl taint nodes --all node-role.kubernetes.io/master- 2>&1) ]] && echo "Masters untainted"
 
 # Ignore if namespace already exists
-[[ ! $(kubectl get ns flux) ]] && kubectl create ns flux
+[[ ! $(kubectl get ns flux 2>&1) ]] && kubectl create ns flux
 
 if [[ -f .secrets/k8s-secret-fluxcd-ssh.yaml ]]; then
     echo "Applying existing SSH key pair"
@@ -37,7 +30,8 @@ helm repo add fluxcd https://charts.fluxcd.io
 
 helm template fluxcd/flux \
     --name-template=default \
-    --namespace flux \
+    --namespace=flux \
+    --version=1.4.1 \
     --values=config/flux/values.yaml > flux.yaml
 
 [[ -f flux.yaml ]] && kubectl apply -f flux.yaml
