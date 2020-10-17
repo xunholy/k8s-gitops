a893dbcc842174f20727e91b08f47a2f6dde23bf
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2020-10-17 12:06:29 +1100

    update toolkit version: 0.1.6
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

---
+++ bootstrap/update.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+VERSION="0.1.6"
+
+if [[ ! $(gotk) ]]; then
+  echo "gotk needs to be installed - https://toolkit.fluxcd.io/get-started/#install-the-toolkit-cli"
+  exit 1
+fi
+
+gotk install \
+  --version="${VERSION}" \
+  --components=source-controller,kustomize-controller,helm-controller,notification-controller \
+  --namespace=gitops-system \
+  --network-policy=false \
+  --arch=arm64 \
+  --export > "./namespaces/gitops-system/toolkit-components.yaml"
+
+git add -A && git commit -sam "update toolkit version: ${VERSION}" && git push
