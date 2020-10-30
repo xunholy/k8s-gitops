17c7c5a5e44776ac24eb9e7ddd723c824d0b972e
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2020-10-30 17:30:42 +1100

    add velero resources and role permissions
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,48 +1,70 @@
-variable "project_name" {}
-variable "billing_account" {}
-variable "org_id" {}
-variable "region" {}
+locals {
+  velero_permissions = [
+    "compute.disks.get",
+    "compute.disks.create",
+    "compute.disks.createSnapshot",
+    "compute.snapshots.get",
+    "compute.snapshots.create",
+    "compute.snapshots.useReadOnly",
+    "compute.snapshots.delete",
+    "compute.zones.get"
+  ]
+}
 
 provider "google" {
   region = var.region
 }
 
 resource "google_storage_bucket" "tfstate" {
-  bucket_policy_only          = "false"
-  default_event_based_hold    = "false"
-  force_destroy               = "false"
-  location                    = "US"
-  name                        = "raspbernetes"
-  project                     = "raspbernetes"
-  requester_pays              = "false"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = "false"
+  default_event_based_hold = "false"
+  force_destroy            = "false"
+  location                 = "ASIA"
+  name                     = "raspbernetes"
+  project                  = "raspbernetes"
+  requester_pays           = "false"
+  storage_class            = "STANDARD"
 
   versioning {
     enabled = "true"
   }
 }
 
+resource "google_service_account" "velero" {
+  account_id   = "velero"
+  display_name = "Service account for Velero"
+}
+
+resource "google_project_iam_custom_role" "velero" {
+  role_id     = "velero"
+  title       = "Velero Role"
+  description = "This role grants all required Velero permissions"
+  permissions = local.velero_permissions
+}
+
+resource "google_service_account_iam_binding" "velero" {
+  service_account_id = google_service_account.velero.name
+  role               = google_project_iam_custom_role.velero.name
+  members = [
+    "serviceAccount:${google_service_account.velero.email}",
+  ]
+}
+
 resource "google_storage_bucket" "velero" {
-  bucket_policy_only          = "false"
-  default_event_based_hold    = "false"
-  force_destroy               = "false"
-  location                    = "US"
-  name                        = "raspbernetes-velero-backups"
-  project                     = "raspbernetes"
-  requester_pays              = "false"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = "false"
+  default_event_based_hold = "false"
+  force_destroy            = "false"
+  location                 = "ASIA"
+  name                     = "raspbernetes-velero-backups"
+  project                  = "raspbernetes"
+  requester_pays           = "false"
+  storage_class            = "STANDARD"
 }
 
 resource "google_storage_bucket" "thanos" {
-  bucket_policy_only          = "false"
-  default_event_based_hold    = "false"
-  force_destroy               = "false"
-  location                    = "ASIA"
-  name                        = "thanos-raspbernetes-storage"
-  project                     = "raspbernetes"
-  requester_pays              = "false"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = "false"
+  default_event_based_hold = "false"
+  force_destroy            = "false"
+  location                 = "ASIA"
+  name                     = "thanos-raspbernetes-storage"
+  project                  = "raspbernetes"
+  requester_pays           = "false"
+  storage_class            = "STANDARD"
 }
