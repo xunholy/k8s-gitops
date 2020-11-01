bb34f384cacad6b00a0961a28c24bb1be4f98f49
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2020-11-01 15:19:13 +1100

    updte TF resources
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

---
+++ infrastructure/gcp/sops.tf
@@ -0,0 +1,13 @@
+resource "google_kms_key_ring" "sops" {
+  name     = "sops"
+  location = "global"
+}
+
+resource "google_kms_crypto_key" "sops" {
+  name     = "sops-key"
+  key_ring = google_kms_key_ring.keyring.id
+  purpose  = ENCRYPT_DECRYPT
+  lifecycle {
+    prevent_destroy = true
+  }
+}
