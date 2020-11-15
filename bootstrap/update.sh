7080b607acedecbbcb2759cf193a13361137fa24
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2020-11-15 14:01:06 +1100

    update.sh fix
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- bootstrap/update.sh
+++ bootstrap/update.sh
@@ -2,20 +2,19 @@
 
 set -eou pipefail
 
-VERSION="0.2.2"
-
 if [[ ! $(flux) ]]; then
   echo "flux needs to be installed - https://toolkit.fluxcd.io/get-started/#install-the-toolkit-cli"
   exit 1
 fi
 
 flux install \
-  --version="${VERSION}" \
+  --version=latest \
   --components=source-controller,kustomize-controller,helm-controller,notification-controller \
   --namespace=flux-system \
   --network-policy=false \
   --arch=arm64 \
+  --log-level=info \
   --export > "./cluster/flux-system/toolkit-components.yaml"
 
 # Uncomment for local useage
-# git add -A && git commit -sam "update toolkit version: ${VERSION}" && git push
+# git add -A && git commit -sam "update toolkit version to latest" && git push
