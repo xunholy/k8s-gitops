6cbc121dc929be47fc3bc71d9d808617142d7320
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2020-11-19 21:36:06 +1100

    Add explicit list of users to grant GCP project permissions
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- infrastructure/gcp/sops.tf
+++ infrastructure/gcp/sops.tf
@@ -1,3 +1,28 @@
+locals {
+  sops_permissions = [
+    "cloudkms.cryptoKeyVersions.useToDecrypt",
+    "cloudkms.cryptoKeyVersions.useToEncrypt",
+    "cloudkms.cryptoKeyVersions.viewPublicKey",
+    "resourcemanager.projects.get"
+  ]
+}
+
+resource "google_project_iam_custom_role" "sops" {
+  role_id     = "sops"
+  title       = "SOPS Role"
+  description = "This role grants all required SOPS permissions"
+  permissions = local.sops_permissions
+}
+
+# Approved list of GCP users with the ability to encrypt/decrypt project secrets
+resource "google_project_iam_binding" "sops" {
+  project = var.project_id
+  role    = google_project_iam_custom_role.sops.name
+  members = [
+    "user:saurabh.c.pandit@gmail.com",
+  ]
+}
+
 resource "google_kms_key_ring" "sops" {
   name     = "sops"
   location = "global"
