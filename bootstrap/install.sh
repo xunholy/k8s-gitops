c935595d6f8e8199c33254cbabadb2629008771e
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2021-02-01 12:13:48 +1100

    updates
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -2,6 +2,11 @@
 
 set -eou pipefail
 
+export GITHUB_TOKEN=
+export GITHUB_USER=xunholy
+export GITHUB_REPO=k8s-gitops
+export CLUSTER="${CLUSTER:-production}"
+
 # KUBECONFIG=~/projects/k8s-cluster-installation/ansible/playbooks/output/k8s-config.yaml:~/.kube/config kubectl config view --flatten > ~/.kube/config.tmp && \
 #   mv ~/.kube/config.tmp ~/.kube/config
 
@@ -10,42 +15,22 @@ if [[ ! $(flux) ]]; then
   exit 1
 fi
 
-# flux bootstrap github \
-#   --owner=xunholy \
-#   --repository=k8s-gitops \
-#   --path=clusters/production \
-#   --branch=main \
-#   --personal
-
 # Untaint master nodes if you don't have enough workers in your homelab
 # [[ ! $(kubectl taint nodes --all node-role.kubernetes.io/master-) ]] && echo "Masters untainted"
-
-# Check the cluster meets the fluxv2 prerequisites
-flux check --pre
-[[ $? -ne 0 ]] && echo "Prerequisites were not satisfied" && exit 1
-
-# flux install \
-#   --version=latest \
-#   --components=source-controller,kustomize-controller,helm-controller,notification-controller \
-#   --namespace=flux-system \
-#   --network-policy=false
 
 if [[ -f .secrets/git-crypt/k8s-secret-sealed-secret-private-key.yaml ]]; then
   echo "Applying existing sealed-secret key"
   kubectl apply -f .secrets/git-crypt/k8s-secret-sealed-secret-private-key.yaml
 fi
 
-if [[ -f clusters/production/gotk-components.yaml ]]; then
-  echo "Applying Components"
-  kubectl apply -f clusters/production/gotk-components.yaml
-fi
+# Check the cluster meets the fluxv2 prerequisites
+flux check --pre
+[[ $? -ne 0 ]] && echo "Prerequisites were not satisfied" && exit 1
 
-if [[ -f clusters/production/gotk-sync.yaml ]]; then
-  echo "Applying Repo Sync"
-  kubectl apply -f clusters/production/gotk-sync.yaml
-fi
-
-if [[ -f clusters/production/kustomization.yaml ]]; then
-  echo "Applying Kustomization"
-  kubectl apply -f clusters/production/kustomization.yaml
-fi
+flux bootstrap github \
+  --owner="${GITHUB_USER}" \
+  --repository="${GITHUB_REPO}" \
+  --path=clusters/"${CLUSTER}" \
+  --branch=main \
+  --network-policy=false \
+  --personal
