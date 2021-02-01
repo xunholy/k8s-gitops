52755a7f2e2998ffd0067ce3f0b9566b31947675
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2021-02-01 11:11:16 +1100

    update dir structure
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -2,8 +2,8 @@
 
 set -eou pipefail
 
-KUBECONFIG=~/projects/k8s-cluster-installation/ansible/playbooks/output/k8s-config.yaml:~/.kube/config kubectl config view --flatten > ~/.kube/config.tmp && \
-  mv ~/.kube/config.tmp ~/.kube/config
+# KUBECONFIG=~/projects/k8s-cluster-installation/ansible/playbooks/output/k8s-config.yaml:~/.kube/config kubectl config view --flatten > ~/.kube/config.tmp && \
+#   mv ~/.kube/config.tmp ~/.kube/config
 
 if [[ ! $(flux) ]]; then
   echo "flux needs to be installed - https://toolkit.fluxcd.io/get-started/#install-the-toolkit-cli"
@@ -17,19 +17,28 @@ fi
 flux check --pre
 [[ $? -ne 0 ]] && echo "Prerequisites were not satisfied" && exit 1
 
-flux install \
-  --version=latest \
-  --components=source-controller,kustomize-controller,helm-controller,notification-controller \
-  --namespace=flux-system \
-  --network-policy=false
+# flux install \
+#   --version=latest \
+#   --components=source-controller,kustomize-controller,helm-controller,notification-controller \
+#   --namespace=flux-system \
+#   --network-policy=false
 
 if [[ -f .secrets/git-crypt/k8s-secret-sealed-secret-private-key.yaml ]]; then
   echo "Applying existing sealed-secret key"
   kubectl apply -f .secrets/git-crypt/k8s-secret-sealed-secret-private-key.yaml
 fi
 
-# TODO: add cluster ARG to script
-if [[ -f clusters/production/repo.yaml ]]; then
+if [[ -f clusters/production/gotk-components.yaml ]]; then
+  echo "Applying Components"
+  kubectl apply -f clusters/production/gotk-components.yaml
+fi
+
+if [[ -f clusters/production/gotk-sync.yaml ]]; then
   echo "Applying Repo Sync"
-  kubectl apply -f clusters/production/repo.yaml
+  kubectl apply -f clusters/production/gotk-sync.yaml
+fi
+
+if [[ -f clusters/production/kustomization.yaml ]]; then
+  echo "Applying Kustomization"
+  kubectl apply -f clusters/production/kustomization.yaml
 fi
