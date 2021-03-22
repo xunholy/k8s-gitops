a8615c901c156ce85842ae4d0f3d86c300fd0b09
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2021-03-23 10:04:08 +1100

    fix: update scripts and fix kustomization errors
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- scripts/validate-kustomize.sh
+++ scripts/validate-kustomize.sh
@@ -9,17 +9,15 @@ kustomize_flags="--enable_kyaml=false --allow_id_changes=false --load_restrictor
 # kustomize_flags="--allow-id-changes=false --load-restrictor=LoadRestrictionsNone"
 kustomize_config="kustomization.yaml"
 
-find "k8s" -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "INFO - Validating $file"
-    yq validate -d'*' "$file"
+find "k8s" -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
+  echo "INFO - Validating $file"
+  yq validate -d'*' "$file"
 done
 
-find "k8s" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "INFO - Validating kustomization ${file/%$kustomize_config}"
-    kustomize build "${file/%$kustomize_config}" $kustomize_flags | kubeval --ignore-missing-schemas
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
-    fi
+find "k8s" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
+  echo "INFO - Validating kustomization ${file/%$kustomize_config}"
+  kustomize build "${file/%$kustomize_config}" $kustomize_flags | kubeval --ignore-missing-schemas
+  if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    exit 1
+  fi
 done
--- scripts/validate-sealed-secrets.sh
+++ scripts/validate-sealed-secrets.sh
@@ -4,12 +4,10 @@ shopt -s extglob
 
 [[ -n $DEBUG ]] && set -x
 
-# shellcheck disable=SC2155
-REPO_ROOT=$(git rev-parse --show-toplevel)
-CLUSTER_ROOT="${REPO_ROOT}/namespaces"
-SEALED_SECRETS_FILES=$(find "$CLUSTER_ROOT" -name '*.encrypted.yaml')
+SEALED_SECRETS_FILES=$(find "k8s" -type f -name '*.encrypted.yaml')
 
 for FILE in $SEALED_SECRETS_FILES; do
-  echo "Validating $FILE"
-  kubeseal --validate --controller-name=sealed-secrets --kubeconfig .kube/config < "$FILE"
+  echo "INFO - Validating $FILE"
+  yq validate -d'*' "$FILE"
+  kubeseal --validate --controller-name=sealed-secrets --kubeconfig ~/.kube/config < "$FILE"
 done
