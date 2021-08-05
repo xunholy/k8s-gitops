6a554147b2f56caabffdd582e7fa3c097b21a7be
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2021-08-05 12:08:49 +1000

    fix: shellcheck errors
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- scripts/validate-kustomize.sh
+++ scripts/validate-kustomize.sh
@@ -47,7 +47,7 @@ done
 echo "INFO - Validating clusters"
 find ./k8s/clusters -type f -name '*.yaml' -maxdepth 1 -print0 | while IFS= read -r -d $'\0' file;
   do
-    kubeval ${file} --strict --ignore-missing-schemas --additional-schema-locations=file:///tmp/flux-crd-schemas
+    kubeval "${file}" --strict --ignore-missing-schemas --additional-schema-locations=file:///tmp/flux-crd-schemas
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
     fi
@@ -57,6 +57,7 @@ echo "INFO - Validating kustomize overlays"
 find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "INFO - Validating kustomization ${file/%$kustomize_config}"
+    # shellcheck disable=SC2086
     kustomize build "${file/%$kustomize_config}" $kustomize_flags | kubeval --ignore-missing-schemas --strict --additional-schema-locations=file:///tmp/flux-crd-schemas
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
