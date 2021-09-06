7408a32df8b24424a1045179e5ed3f0c29bc3e39
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2021-09-07 09:47:18 +1000

    fix: validation script
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- scripts/validate-kustomize.sh
+++ scripts/validate-kustomize.sh
@@ -57,8 +57,9 @@ echo "INFO - Validating kustomize overlays"
 find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "INFO - Validating kustomization ${file/%$kustomize_config}"
+    # Secrets are ignored with --skip-kinds due to using SOPS with FluxCD
     # shellcheck disable=SC2086
-    kustomize build "${file/%$kustomize_config}" $kustomize_flags | kubeval --ignore-missing-schemas --strict --additional-schema-locations=file:///tmp/flux-crd-schemas
+    kustomize build "${file/%$kustomize_config}" $kustomize_flags | kubeval --ignore-missing-schemas --strict --additional-schema-locations=file:///tmp/flux-crd-schemas --skip-kinds Secret
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
     fi
