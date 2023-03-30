882e35a05809c6ddf7855afda464209190204d6d
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2023-03-30 21:42:43 +1100

    feat: add cloudflare terraform
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- terraform/_backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "raspbernetes-gitops-terraform-state"
-    prefix = "terraform/state"
-  }
-}
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  description = "The GCP project ID that resources will be created in"
-  default     = "raspbernetes"
-}
-
-variable "region" {
-  description = "The GCP region that resources will be created in"
-  default     = "us-central1"
-}
