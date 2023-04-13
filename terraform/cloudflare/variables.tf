19357135470664175b9a8285c37768e7af9e00f3
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2023-04-14 09:23:41 +1000

    fix: lint workflow
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
    
    fix: lint workflow
    
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
    
    fix: lint workflow
    
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
    
    fix: lint issues
    
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- terraform/cloudflare/variables.tf
+++ terraform/cloudflare/variables.tf
@@ -7,11 +7,11 @@ variable "domain" {
 variable "kubernetes_cluster_api" {
   description = "(OPTIONAL) The Kubernetes cluster API endpoint to use for the cluster. (default: https://api.raspbernetes.com)"
   default     = "api.raspbernetes.com"
-  type = string
+  type        = string
 }
 
 variable "session_duration" {
   description = "(OPTIONAL) The session duration for the cluster. (default: 24h)"
-  default = "24h"
-  type = string
+  default     = "24h"
+  type        = string
 }
--- terraform/gcp/variables.tf
+++ terraform/gcp/variables.tf
@@ -1,9 +1,11 @@
 variable "project_id" {
   description = "(OPTIONAL) The GCP project ID to use for the cluster. (default: raspbernetes)"
   default     = "raspbernetes"
+  type        = string
 }
 
 variable "region" {
   description = "(OPTIONAL) The GCP region to use for the cluster. (default: us-central1)"
   default     = "us-central1"
+  type        = string
 }
