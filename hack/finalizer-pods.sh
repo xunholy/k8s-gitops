6eea27e676ff38668b10e4d2c610408cadb398c4
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2025-02-27 11:58:09 +1100

    fix: kustomization path for mutating webhook
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- hack/finalizer-pods.sh
+++ hack/finalizer-pods.sh
@@ -13,11 +13,3 @@ for NAMESPACE_WITH_PREFIX in $NAMESPACES; do
   # Update the finalizer for each Pod in parallel using xargs
   echo "$POD_NAMES" | xargs -n1 -P4 -I{} kubectl patch {} -n $NAMESPACE -p '{"metadata":{"finalizers":null}}' --type=merge
 done
-
-
-
-
-flux-system   namespaces    10m   False
-kustomize build failed: accumulating resources: accumulation
-err='merging resources from '../../base/kube-system/cilium/ks.yaml': may not add resource with an already registered id: Kustomization.v1.kustomize.toolkit.fluxcd.io/cilium.flux-system':
-must build at directory: '/tmp/kustomization-548993612/namespaces/base/kube-system/cilium/ks.yaml': file is not directory
